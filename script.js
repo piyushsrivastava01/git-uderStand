4f2328e42610808f70e89d6262359d193d5c1ab1
piyushshrivastava01 <piyushsrivastava1009@gmail.com>
2024-03-03 13:38:14 +0530

    this is a tutorial of git

---
+++ script.js
@@ -0,0 +1,6 @@
+let btn = document.querySelector(".btn")
+let heading = document.querySelector("h1")
+
+btn.addEventListener("click", function(){
+    heading.innerHTML = "its js logic of implementation is working now !"
+})
